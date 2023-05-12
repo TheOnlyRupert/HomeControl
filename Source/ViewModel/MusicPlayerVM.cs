5e52a25503a658884e11b7125d696d65e924b3a5
Robert Higgins <3703153+TheOnlyRupert@users.noreply.github.com>
2023-05-12 15:46:56 -0500

    More Work on MediaPlayer
    More work on MediaPlayer

--- Source/ViewModel/MusicPlayerVM.cs
+++ Source/ViewModel/MusicPlayerVM.cs
@@ -10,11 +10,17 @@ using HomeControl.Source.ViewModel.Base;
 namespace HomeControl.Source.ViewModel;
 
 public class MusicPlayerVM : BaseViewModel {
+    private string _songName, _shuffleIcon, _artistName, _timer1Text, _timer2Text;
+    private int _progressValue;
+
     public MusicPlayerVM() {
         SpotifyToken token = GetAccessToken();
         try {
-            GetPlayLists(token.access_token, "");
-        } catch (Exception) { }
+            Playlist playlist = GetPlayLists(token.access_token, "1");
+            Console.WriteLine(playlist.href);
+        } catch (Exception e) {
+            Console.WriteLine(e);
+        }
     }
 
     public ICommand ButtonCommand => new DelegateCommand(ButtonCommandLogic, true);
@@ -26,12 +32,13 @@ public class MusicPlayerVM : BaseViewModel {
 
             WebRequest request = WebRequest.Create("https://accounts.spotify.com/api/token");
             request.Method = "POST";
-            request.Headers.Add("Authorization", "Basic YmRjYzZiMDdiNmE1NDAwZWFlN2Y5MDRmNmM2NzE0Y2I6ZTBiNzIwY2VkYzM1NDg4MzlhMjdkZGYyMGIxZDU1ZjQ");
+            request.Headers.Add("Authorization", "Basic YmRjYzZiMDdiNmE1NDAwZWFlN2Y5MDRmNmM2NzE0Y2I6M2I4NzYzYmFkNTdiNGFjNWE1Mjc4NjgyMzI1NzIwODQ");
             request.ContentType = "application/x-www-form-urlencoded";
             request.ContentLength = byteArray.Length;
 
             Stream dataStream = request.GetRequestStream();
             dataStream.Write(byteArray, 0, byteArray.Length);
+            
             WebResponse response = request.GetResponse();
             Stream responseStream = response.GetResponseStream();
             StreamReader reader = new(responseStream);
@@ -54,9 +61,9 @@ public class MusicPlayerVM : BaseViewModel {
             request.Headers.Add("Authorization", "Bearer " + token);
             request.ContentType = "application/json; charset=utf-8";
 
-            using WebResponse response = request.GetResponse();
-            using Stream dataStream = response.GetResponseStream();
-            using StreamReader reader = new(dataStream);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new(dataStream);
             string responseFromServer = reader.ReadToEnd();
 
             Console.WriteLine(responseFromServer);
@@ -66,8 +73,9 @@ public class MusicPlayerVM : BaseViewModel {
             };
 
             return JsonSerializer.Deserialize<T>(responseFromServer, options);
-        } catch (Exception) {
-            return default;
+        } catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
         }
     }
 
@@ -83,4 +91,56 @@ public class MusicPlayerVM : BaseViewModel {
             break;
         }
     }
+
+    #region Fields
+
+    public string SongName {
+        get => _songName;
+        set {
+            _songName = value;
+            RaisePropertyChangedEvent("SongName");
+        }
+    }
+    
+    public string ShuffleIcon {
+        get => _shuffleIcon;
+        set {
+            _shuffleIcon = value;
+            RaisePropertyChangedEvent("ShuffleIcon");
+        }
+    }
+    
+    public string ArtistName {
+        get => _artistName;
+        set {
+            _artistName = value;
+            RaisePropertyChangedEvent("ArtistName");
+        }
+    }
+    
+    public string Timer1Text {
+        get => _timer1Text;
+        set {
+            _timer1Text = value;
+            RaisePropertyChangedEvent("Timer1Text");
+        }
+    }
+    
+    public string Timer2Text {
+        get => _timer2Text;
+        set {
+            _timer2Text = value;
+            RaisePropertyChangedEvent("Timer2Text");
+        }
+    }   
+    
+    public int ProgressValue {
+        get => _progressValue;
+        set {
+            _progressValue = value;
+            RaisePropertyChangedEvent("ProgressValue");
+        }
+    }
+
+    #endregion
 }
